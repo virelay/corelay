398969136d8f007521cd588d5ba57f21b305d1ab
chrstphr <chrstphr@posteo.eu>
2019-08-23 14:20:27 +0200

    added proper docstring, added some error checking for more stable flow Processors

--- sprincl/processor/flow.py
+++ sprincl/processor/flow.py
@@ -1,49 +1,141 @@
-from typing import Iterable as IterableBase
-
 from ..base import Param
 from .base import Processor
-
-
-class IterableMeta(type):
-    def __instancecheck__(self, instance):
-        return isinstance(instance, IterableBase) and all(isinstance(obj, self.__subtype__) for obj in instance)
-
-
-class Iterable(metaclass=IterableMeta):
-    __subtype__ = object
-
-    def __class_getitem__(cls, params):
-        if not isinstance(params, tuple):
-            params = (params,)
-        if not params:
-            raise TypeError("At least one subtypes must be specified!")
-        if not all(isinstance(obj, type) for obj in params):
-            raise TypeError("Subtypes must be types!")
-        return type('{}[{}]'.format(cls.__name__, params), (cls,), {'__subtype__': params})
+from ..util import zip_equal, Iterable
 
 
 class Shaper(Processor):
-    shape = Param(Iterable[int])
+    """Extracts and/ or copies indices.
+
+    Attributes
+    ----------
+    indices : iterable of (int or iterable of int)
+        Iterable of indices to copy/ extract. The resuling output will be a tuple with the same member shape. Each index
+        may be passed an arbitrary amount of times. Outer tuples allow ints and tuples, inner tuples only allow ints.
+
+    Examples
+    -------
+    >>> Shaper(indices=(0, 1, (0, 1, 2)))(['a', 'b', 'c'])
+    ('a', 'b', ('a', 'b', 'c'))
+
+    """
+    indices = Param(Iterable[int, Iterable[int]])
 
     def function(self, data):
-        return tuple(data[index] for index in self.shape)
+        """Extracts and/ or copies indices of data.
+
+        Parameters
+        ----------
+        data : object
+            Object from which to extract/ copy elements. If not an iterable, index 0 corresponds to the object itself.
+
+        Returns
+        -------
+        tuple of object
+            The extracted/ copied indices of data.
+
+        Raises
+        ------
+        TypeError
+            If an invalid index was accessed for data.
+
+        """
+        def extract_indices(iterable, shape):
+            """Recursively extract indices"""
+            result = []
+            for index in shape:
+                if isinstance(index, Iterable):
+                    obj = extract_indices(iterable, index)
+                else:
+                    try:
+                        obj = data[index]
+                    except KeyError as err:
+                        raise TypeError("'{}' is not a valid index for '{}'".format(index, data)) from err
+                result.append(obj)
+            return result
+
+        if not isinstance(data, Iterable):
+            data = (data,)
+        result = extract_indices(data, self.shape)
+        return tuple(result)
 
 
 class GroupProcessor(Processor):
-    children = Param(Iterable[Processor], default=tuple())
+    """Abstract class for groups of Processors.
+
+    Attributes
+    ----------
+    children : iterable of :obj:`Processor`
+        Child Processors for this group.
+
+    """
+    children = Param(Iterable[Processor])
 
 
 class Parallel(GroupProcessor):
+    """Processor group calling its children in Parallel.
+
+    Examples
+    --------
+    >>> Parallel(children=[FunctionProcessor(function=lambda x: x**n) for n in (1, 2, 3, 4)])((2, 2, 2, 2))
+    [2, 4, 8, 16]
+    >>> Parallel(children=[FunctionProcessor(function=lambda x: x**n) for n in (1, 2, 3, 4)])(2)
+    [2, 4, 8, 16]
+
+    """
+
     def function(self, data):
+        """Sequentially get one element from data per child, call all children with this element as input in parallel,
+        and accumulate the outputs.
+
+        Parameters
+        ----------
+        data : iterable or object
+            Iterable from which to pass the elements to the children. If data is not an Iterable, it will be copied as
+            many times as there are children.
+
+        Raises
+        ------
+        TypeError
+            If the number of children and number of data elements mismatch.
+
+        """
+        def wrap_err(iterator):
+            """Wraps around iterator to raise correct error"""
+            try:
+                yield next(iterator)
+            except TypeError as err:
+                raise TypeError("Number of data elements and children does not match!") from err
+            except StopIteration:
+                pass
+
+        if not isinstance(data, Iterable):
+            data = (data for _ in self.children)
+
         result = []
-        for child, element in zip(self.children, data):
+        for child, element in wrap_err(zip_equal(self.children, data)):
             out = child(element)
             result.append(out)
         return result
 
 
 class Sequential(GroupProcessor):
+    """Processor group calling its children in Parallel.
+
+    Examples
+    --------
+    >>> Parallel(children=[FunctionProcessor(function=lambda x: c + x) for c in 'abcd'])('=')
+    'dcba='
+
+    """
     def function(self, data):
+        """Feed data forward sequentially, passing each child's output to the next child.
+
+            Parameters
+            ----------
+            data : object
+                Input data to pass to the first child.
+
+        """
         for child in self.children:
             data = child(data)
         return data
