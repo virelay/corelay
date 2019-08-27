8dc5dfd79fd1784a0e1bd9f9df85eec2b77bb906
chrstphr <chrstphr@posteo.eu>
2019-08-27 14:20:58 +0200

    added tests for shaper

---
+++ tests/sprincl/processor/test_flow.py
@@ -0,0 +1,43 @@
+import pytest
+
+from sprincl.processor.flow import Shaper
+
+
+class TestShaper:
+    @staticmethod
+    def test_extract():
+        """Extracting a single element should succeed"""
+        shaper = Shaper(indices=(1,))
+        assert shaper([1, 2, 3]) == 2
+
+    @staticmethod
+    def test_extract_multi():
+        """Extracting multiple elements should succeed"""
+        shaper = Shaper(indices=(1, 2))
+        assert shaper([1, 2, 3]) == (2, 3)
+
+    @staticmethod
+    def test_copy():
+        """Specifying the same index multiple times should succeed"""
+        shaper = Shaper(indices=(1, 2, 1))
+        assert shaper([1, 2, 3]) == (2, 3, 2)
+
+    @staticmethod
+    def test_stacked():
+        """Using another inner tuple for specifying indices should succeed"""
+        shaper = Shaper(indices=(1, 2, (0, 2)))
+        assert shaper([1, 2, 3]) == (2, 3, (1, 3))
+
+    @staticmethod
+    def test_stacked_too_much():
+        """Specifying tuples any deeper than depth 2 should fail"""
+        with pytest.raises(TypeError):
+            Shaper(indices=(0, (1, (2,))))
+
+
+class TestParallel:
+    pass
+
+
+class TestSequential:
+    pass
